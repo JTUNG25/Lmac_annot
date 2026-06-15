427879402bb193328d140f8bcb577891eed0bee0
JaneTung <jane.tung@uq.edu.au>
2026-06-15 11:39:16 +1000

    module added

--- run_nf_tiberius.sh
+++ run_nf_tiberius.sh
@@ -2,15 +2,20 @@
 #SBATCH --account=a_qaafi_chs
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
 #SBATCH --time=48:00:00
-#SBATCH --job-name=ti=tb_evidence
+#SBATCH --job-name=tb_evidence
 #SBATCH --partition=general
 #SBATCH --qos=normal
 #SBATCH --output=tbe.log
 
+source /sw/local/rocky8/noarch/rcc/software/miniforge/24.11.3-0/etc/profile.d/conda.sh
+conda activate snakemake8
+
 module load nextflow/25.04.6
+module load java/21.0.8
+module load sra-toolkit/3.0.10-gompi-2023a
 
 export APPTAINER_TMPDIR=/scratch/user/uqctung/tmp
 export APPTAINER_CACHEDIR=/scratch/user/uqctung/cache
@@ -21,10 +26,6 @@ mkdir -p $NXF_WORK
 
 cd /QRISdata/Q9140/lmac/annot_lmac
 
-apptainer exec \
-    --nv \
-    -B $PWD,$NXF_WORK,$APPTAINER_TMPDIR \
-    /home/uqctung/containers/tiberius_latest.sif \
-    python /opt/Tiberius/tiberius.py \
+python Tiberius/tiberius.py \
     --params_yaml profiles/bunya/nf_params.yaml \
     --nf_config profiles/bunya/nf.config
--- run_tiberius.sh
+++ run_tiberius.sh
@@ -4,26 +4,16 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
-#SBATCH --time=48:00:00
-#SBATCH --job-name=tb_evidence
+#SBATCH --time=24:00:00
+#SBATCH --job-name=tiberius
 #SBATCH --partition=general
 #SBATCH --qos=normal
-#SBATCH --output=tbe.log
+#SBATCH --output=tb.log
 
 source /sw/local/rocky8/noarch/rcc/software/miniforge/24.11.3-0/etc/profile.d/conda.sh
 conda activate snakemake8
 
-module load nextflow/25.04.6
+export TMPDIR=/scratch/user/uqctung/tmp
+mkdir -p $TMPDIR
 
-export APPTAINER_TMPDIR=/scratch/user/uqctung/tmp
-export APPTAINER_CACHEDIR=/scratch/user/uqctung/cache
-mkdir -p $APPTAINER_TMPDIR $APPTAINER_CACHEDIR
-
-export NXF_WORK=/scratch/user/uqctung/nextflow_work
-mkdir -p $NXF_WORK
-
-cd /QRISdata/Q9140/lmac/annot_lmac
-
-python Tiberius/tiberius.py \
-    --params_yaml profiles/bunya/nf_params.yaml \
-    --nf_config profiles/bunya/nf.config
+snakemake -s tiberius.smk --profile profiles/bunya/
