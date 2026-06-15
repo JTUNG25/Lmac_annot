a0319f18a71e5f2ad4b0106dd507ef4f4897e34a
JaneTung <jane.tung@uq.edu.au>
2026-06-15 11:30:32 +1000

    container issue

--- run_tiberius.sh
+++ run_tiberius.sh
@@ -4,16 +4,26 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
-#SBATCH --time=24:00:00
-#SBATCH --job-name=tiberius
+#SBATCH --time=48:00:00
+#SBATCH --job-name=tb_evidence
 #SBATCH --partition=general
 #SBATCH --qos=normal
-#SBATCH --output=tb.log
+#SBATCH --output=tbe.log
 
 source /sw/local/rocky8/noarch/rcc/software/miniforge/24.11.3-0/etc/profile.d/conda.sh
 conda activate snakemake8
 
-export TMPDIR=/scratch/user/uqctung/tmp
-mkdir -p $TMPDIR
+module load nextflow/25.04.6
 
-snakemake -s tiberius.smk --profile profiles/bunya/
+export APPTAINER_TMPDIR=/scratch/user/uqctung/tmp
+export APPTAINER_CACHEDIR=/scratch/user/uqctung/cache
+mkdir -p $APPTAINER_TMPDIR $APPTAINER_CACHEDIR
+
+export NXF_WORK=/scratch/user/uqctung/nextflow_work
+mkdir -p $NXF_WORK
+
+cd /QRISdata/Q9140/lmac/annot_lmac
+
+python Tiberius/tiberius.py \
+    --params_yaml profiles/bunya/nf_params.yaml \
+    --nf_config profiles/bunya/nf.config
